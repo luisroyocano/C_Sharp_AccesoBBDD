d627ef0205dd3caa699a3f901637be1fcc5b40f1
fraxito <fraxito@gmail.com>
2019-05-06 00:09:52 +0200

    corregido el cierre de la app

--- EjemploAccesoBBDDMysqlCSharp/VentanaPrincipal.cs
+++ EjemploAccesoBBDDMysqlCSharp/VentanaPrincipal.cs
@@ -20,21 +20,9 @@ namespace EjemploAccesoBBDDMysqlCSharp
         protected override void OnFormClosing(FormClosingEventArgs e)
         {
             base.OnFormClosing(e);
+            
+            Application.Exit();
 
-            if (e.CloseReason == CloseReason.WindowsShutDown) return;
-
-            // Confirm user wants to close
-            switch (MessageBox.Show(this, "Are you sure you want to close?", "Closing", MessageBoxButtons.YesNo))
-            {
-                case DialogResult.No:
-                    e.Cancel = true;
-                    break;
-                case DialogResult.Yes:
-                    Application.Exit();
-                    break;
-                default:
-                    break;
-            }
         }
     }
 }
